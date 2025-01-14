1539b84ae836b69abf1e7474e8e4aa905e95dd10
EngineerShark <ebert.tylerpedro@gmail.com>
2025-01-14 11:47:36 +1000

    important

--- src/main/java/net/engineershark/tideturner/TideTurner.java
+++ src/main/java/net/engineershark/tideturner/TideTurner.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// very important comment
 public class TideTurner implements ModInitializer {
 	public static final String MOD_ID = "tideturner";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
