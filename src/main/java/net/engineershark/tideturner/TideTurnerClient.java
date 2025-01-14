0a71a28d888c84cda55a8dea2e2a6cc050f44b92
EngineerShark <ebert.tylerpedro@gmail.com>
2025-01-14 11:45:20 +1000

    Initial commit

---
+++ src/main/java/net/engineershark/tideturner/TideTurnerClient.java
@@ -0,0 +1,10 @@
+package net.engineershark.tideturner;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TideTurnerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
